9313ac9355a3d1429a6ec9fabbcd0ded2a028e23
Guiorgy <Guiorgy123@gmail.com>
2025-05-25 22:23:26 +0400

    add template params to remove for compiler hinting

--- include/lrucache.hpp
+++ include/lrucache.hpp
@@ -1961,72 +1961,188 @@ namespace guiorgy {
 		// If the key exists in the container, removes the value that is mapped to the
 		// given key and returns a std::optional with the removed value,
 		// otherwise, returns an empty optional.
+		// Use key_exists to hint to the compiler for which case to optimize for.
+		template<const Likelihood key_exists = Likelihood::Unknown>
 		[[nodiscard("Use erase(const key_t& key) instead")]]
 		std::optional<value_t> remove(const key_t& key) {
 			map_iterator_t it = this->_cache_items_map.find(key);
 
-			if (it != this->_cache_items_map.end()) {
-				value_t& value = this->_cache_items_list._erase_value_at(it->second).second;
-				this->_cache_items_map.erase(it);
-				return value;
-			} else {
-				return std::nullopt;
+			static_assert(is_valid_likelihood(key_exists), "key_exists has an invalid enum value for Likelihood");
+			if constexpr (key_exists == Likelihood::Unknown) {
+				if (it != this->_cache_items_map.end()) {
+					value_t& value = this->_cache_items_list._erase_value_at(it->second).second;
+					this->_cache_items_map.erase(it);
+					return value;
+				} else {
+					return std::nullopt;
+				}
+			} else if constexpr (key_exists == Likelihood::Likely) {
+				if (it != this->_cache_items_map.end()) LIKELY {
+					value_t& value = this->_cache_items_list._erase_value_at(it->second).second;
+					this->_cache_items_map.erase(it);
+					return value;
+				} else {
+					return std::nullopt;
+				}
+			} else if constexpr (key_exists == Likelihood::Unlikely) {
+				if (it != this->_cache_items_map.end()) UNLIKELY {
+					value_t& value = this->_cache_items_list._erase_value_at(it->second).second;
+					this->_cache_items_map.erase(it);
+					return value;
+				} else {
+					return std::nullopt;
+				}
 			}
+		}
+
+		// See the remove above for details.
+		// Use key_likely_exists to hint to the compiler for which case to optimize for.
+		template<const bool key_likely_exists>
+		[[nodiscard("Use erase(const key_t& key) instead")]]
+		std::optional<value_t> remove(const key_t& key) {
+			return remove<likelihood(key_likely_exists)>(key);
 		}
 
 		// If the key exists in the container, removes the value that is mapped to the
 		// given key and returns a std::optional with a reference to the removed value,
 		// otherwise, returns an empty optional.
+		// Use key_exists to hint to the compiler for which case to optimize for.
 		// Remarks:
 		//   - No guarantees are given about the underlying object lifetime when
 		//     modifying the cache (inserting/removing elements), so use with caution.
+		template<const Likelihood key_exists = Likelihood::Unknown>
 		[[nodiscard("Use erase(const key_t& key) instead")]]
 		std::optional<std::reference_wrapper<value_t>> remove_ref(const key_t& key) {
 			map_iterator_t it = this->_cache_items_map.find(key);
 
-			if (it != this->_cache_items_map.end()) {
-				value_t& value = this->_cache_items_list._erase_value_at(it->second).second;
-				this->_cache_items_map.erase(it);
-				return std::make_optional(std::ref(value));
-			} else {
-				return std::nullopt;
+			static_assert(is_valid_likelihood(key_exists), "key_exists has an invalid enum value for Likelihood");
+			if constexpr (key_exists == Likelihood::Unknown) {
+				if (it != this->_cache_items_map.end()) {
+					value_t& value = this->_cache_items_list._erase_value_at(it->second).second;
+					this->_cache_items_map.erase(it);
+					return std::make_optional(std::ref(value));
+				} else {
+					return std::nullopt;
+				}
+			} else if constexpr (key_exists == Likelihood::Likely) {
+				if (it != this->_cache_items_map.end()) LIKELY {
+					value_t& value = this->_cache_items_list._erase_value_at(it->second).second;
+					this->_cache_items_map.erase(it);
+					return std::make_optional(std::ref(value));
+				} else {
+					return std::nullopt;
+				}
+			} else if constexpr (key_exists == Likelihood::Unlikely) {
+				if (it != this->_cache_items_map.end()) UNLIKELY {
+					value_t& value = this->_cache_items_list._erase_value_at(it->second).second;
+					this->_cache_items_map.erase(it);
+					return std::make_optional(std::ref(value));
+				} else {
+					return std::nullopt;
+				}
 			}
+		}
+
+		// See the remove_ref above for details.
+		// Use key_likely_exists to hint to the compiler for which case to optimize for.
+		template<const bool key_likely_exists>
+		[[nodiscard("Use erase(const key_t& key) instead")]]
+		std::optional<std::reference_wrapper<value_t>> remove_ref(const key_t& key) {
+			return remove_ref<likelihood(key_likely_exists)>(key);
 		}
 
 		// If the key exists in the container, removes the value that is mapped to the
 		// given key, returns true and copies the value that is mapped to the given key
 		// into the given value_out reference, or false if such key does not exist.
+		// Use key_exists to hint to the compiler for which case to optimize for.
+		template<const Likelihood key_exists = Likelihood::Unknown>
 		[[nodiscard("Use erase(const key_t& key) instead")]]
 		bool try_remove(const key_t& key, value_t& value_out) {
 			map_iterator_t it = this->_cache_items_map.find(key);
 
-			if (it != this->_cache_items_map.end()) {
-				value_out = this->_cache_items_list._erase_value_at(it->second).second;
-				this->_cache_items_map.erase(it);
-				return true;
-			} else {
-				return false;
+			static_assert(is_valid_likelihood(key_exists), "key_exists has an invalid enum value for Likelihood");
+			if constexpr (key_exists == Likelihood::Unknown) {
+				if (it != this->_cache_items_map.end()) {
+					value_out = this->_cache_items_list._erase_value_at(it->second).second;
+					this->_cache_items_map.erase(it);
+					return true;
+				} else {
+					return false;
+				}
+			} else if constexpr (key_exists == Likelihood::Likely) {
+				if (it != this->_cache_items_map.end()) LIKELY {
+					value_out = this->_cache_items_list._erase_value_at(it->second).second;
+					this->_cache_items_map.erase(it);
+					return true;
+				} else {
+					return false;
+				}
+			} else if constexpr (key_exists == Likelihood::Unlikely) {
+				if (it != this->_cache_items_map.end()) UNLIKELY {
+					value_out = this->_cache_items_list._erase_value_at(it->second).second;
+					this->_cache_items_map.erase(it);
+					return true;
+				} else {
+					return false;
+				}
 			}
+		}
+
+		// See the try_remove above for details.
+		// Use key_likely_exists to hint to the compiler for which case to optimize for.
+		template<const bool key_likely_exists>
+		[[nodiscard("Use erase(const key_t& key) instead")]]
+		bool try_remove(const key_t& key, value_t& value_out) {
+			return try_remove<likelihood(key_likely_exists)>(key, value_out);
 		}
 
 		// If the key exists in the container, removes the value that is mapped to the
 		// given key, returns true and assigns the address to the value that is mapped
 		// to the given key into the given value_out pointer reference, or false if
 		// such key does not exist.
+		// Use key_exists to hint to the compiler for which case to optimize for.
 		// Remarks:
 		//   - No guarantees are given about the underlying object lifetime when
 		//     modifying the cache (inserting/removing elements), so use with caution.
+		template<const Likelihood key_exists = Likelihood::Unknown>
 		[[nodiscard("Use erase(const key_t& key) instead")]]
 		bool try_remove_ref(const key_t& key, const value_t*& value_out) {
 			map_iterator_t it = this->_cache_items_map.find(key);
 
-			if (it != this->_cache_items_map.end()) {
-				value_out = &(this->_cache_items_list._erase_value_at(it->second).second);
-				this->_cache_items_map.erase(it);
-				return true;
-			} else {
-				return false;
+			static_assert(is_valid_likelihood(key_exists), "key_exists has an invalid enum value for Likelihood");
+			if constexpr (key_exists == Likelihood::Unknown) {
+				if (it != this->_cache_items_map.end()) {
+					value_out = &(this->_cache_items_list._erase_value_at(it->second).second);
+					this->_cache_items_map.erase(it);
+					return true;
+				} else {
+					return false;
+				}
+			} else if constexpr (key_exists == Likelihood::Likely) {
+				if (it != this->_cache_items_map.end()) LIKELY {
+					value_out = &(this->_cache_items_list._erase_value_at(it->second).second);
+					this->_cache_items_map.erase(it);
+					return true;
+				} else {
+					return false;
+				}
+			} else if constexpr (key_exists == Likelihood::Unlikely) {
+				if (it != this->_cache_items_map.end()) UNLIKELY {
+					value_out = &(this->_cache_items_list._erase_value_at(it->second).second);
+					this->_cache_items_map.erase(it);
+					return true;
+				} else {
+					return false;
+				}
 			}
+		}
+
+		// See the try_remove_ref above for details.
+		// Use key_likely_exists to hint to the compiler for which case to optimize for.
+		template<const bool key_likely_exists>
+		[[nodiscard("Use erase(const key_t& key) instead")]]
+		bool try_remove_ref(const key_t& key, const value_t*& value_out) {
+			return try_remove_ref<likelihood(key_likely_exists)>(key, value_out);
 		}
 
 		// If the key exists in the container, removes the value that is mapped to the
