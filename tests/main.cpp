3dea029a3c13c3a2ef1a8d961d5c6373a085022e
Guiorgy <Guiorgy123@gmail.com>
2025-05-25 16:57:51 +0400

    color the configuration output in yellow
    also fixed a typo in the output (tets -> tests)

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,10 +1,12 @@
 #include "gtest/gtest.h"
 
 int main(int argc, char **argv) {
+	static constexpr const char* reset_color = "\033[0m";
+	static constexpr const char* bright_yellow_color = "\033[93m";
 #ifdef NDEBUG
-	std::cout << "Running tets in Release configuration\n";
+	std::cout << bright_yellow_color << "Running tests in Release configuration" << reset_color << '\n';
 #else
-	std::cout << "Running tets in Debug configuration\n";
+	std::cout << bright_yellow_color << "Running tests in Debug configuration" << reset_color << '\n';
 #endif
 
 	::testing::InitGoogleTest(&argc, argv);
