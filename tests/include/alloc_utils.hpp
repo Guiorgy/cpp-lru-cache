2fd9511f3f6fef1a9bd06b83bd2f887ad651b763
Guiorgy <Guiorgy123@gmail.com>
2025-06-19 22:26:26 +0400

    fixed forwarding of __VA_ARGS__ on MSVC
    the two arguments to EXPECT_ALLOC_COUNT_AND_BYTES were being treated as a single argument on MSVC

--- tests/include/alloc_utils.hpp
+++ tests/include/alloc_utils.hpp
@@ -8,6 +8,12 @@ extern std::atomic<std::size_t> allocated_bytes;
 
 void extern reset_allocation_count() noexcept;
 
+// The identity/expansion macro to force an extra scan. Necessary on MSVC.
+#define IDENTITY(x) x
+
+// Takes a macro name and its arguments, and forwards them.
+#define FORWARD_ARGS(macro, ...) IDENTITY(macro(__VA_ARGS__))
+
 #define EXPECT_NO_ALLOC() \
 	do { EXPECT_EQ(0u, allocation_count.load()); EXPECT_EQ(0u, allocated_bytes.load()); } while (false)
 
@@ -20,8 +26,8 @@ void extern reset_allocation_count() noexcept;
 #define EXPECT_ALLOC_COUNT_AND_BYTES(expected_count, expected_bytes) \
 	do { EXPECT_EQ(expected_count, allocation_count.load()); EXPECT_EQ(expected_bytes, allocated_bytes.load()); } while (false)
 
-#define GET_EXPECT_ALLOC_MACRO(_1, _2, NAME, ...) NAME
+#define GET_EXPECT_ALLOC_MACRO(_1, _2, macro, ...) macro
 
 #define EXPECT_ALLOC(...) \
-	GET_EXPECT_ALLOC_MACRO(__VA_ARGS__, EXPECT_ALLOC_COUNT_AND_BYTES, EXPECT_ALLOC_COUNT)(__VA_ARGS__)
+	FORWARD_ARGS(GET_EXPECT_ALLOC_MACRO(__VA_ARGS__, EXPECT_ALLOC_COUNT_AND_BYTES, EXPECT_ALLOC_COUNT), __VA_ARGS__)
 #endif // SKIP_ALLOCATION_TESTS
